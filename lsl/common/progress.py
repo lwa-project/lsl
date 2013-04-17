36d385cf11f017fbf5dba12b4c558e15dcddedf2
Jayce Dowell <jayce.dowell@gmail.com>
2013-04-17 23:12:29 +0000

    Applied r1266 to the development trunk (new progress bar).

--- lsl/common/progress.py
+++ lsl/common/progress.py
@@ -5,10 +5,11 @@ Module to make an ASCII progress bar.
 """
 
 import copy
+import time
 
-__version__ = '0.1'
+__version__ = '0.2'
 __revision__ = '$Rev$'
-__all__ = ['ProgressBar', '__version__', '__revision__', '__all__']
+__all__ = ['ProgressBar', 'ProgressBarPlus', '__version__', '__revision__', '__all__']
 
 
 class ProgressBar(object):
@@ -142,3 +143,106 @@ class ProgressBar(object):
 		"""
 
 		return self.show()
+
+
+class ProgressBarPlus(ProgressBar):
+	"""
+	Extended version of the ProgressBar class that has a crude time 
+	estimator.
+	
+	Example Usage:
+		>>> import sys
+		>>> from progess import ProgressBar
+		>>> pb = ProgressBar()
+		>>> pb.inc()
+		>>> sys.stdout.write(pb.show())
+		>>> sys.stdout.flush()
+		
+	.. note::
+		The timing feature is only active when the inc()/dec() functions are called.
+		
+	.. versionadded:: 0.6.4
+	"""
+	
+	t0 = None
+	t1 = None
+	
+	def inc(self, amount=1):
+		"""
+		Increment the progress bar's internal counter by some amount.  The
+		default is one.
+		"""
+
+		self.__iadd__(amount)
+		
+		if self.t0 is None:
+			self.t0 = time.time()
+		self.t1 = time.time()
+		
+	def dec(self, amount=1):
+		"""
+		Decrement the progress bar's internal counter by some amount.  The
+		default is one.
+		"""
+			
+		self.__isub__(amount)
+		if self.t0 is None:
+			self.t0 = time.time()
+		self.t1 = time.time()
+		
+	def show(self):
+		"""
+		Build a string representation of the progress bar and return it.
+		"""
+		
+		if self.t0 is None:
+			# Have we started?
+			cte = '----m--s'
+		elif self.t1 - self.t0 < 0.2:
+			# Have we running long enough to get a "good" estimate?
+			cte = '----m--s'
+		elif self.amount == 0:
+			# Have we gone far enough to get a "good" estimate?
+			cte = '----m--s'
+		elif self.amount == self.max:
+			# Are we done?
+			cte = self.t1 - self.t0
+			cte = '%4im%02is' % (cte/60, cte%60)
+		else:
+			cte = (self.max - self.amount)* self.amount/(self.t1 - self.t0)
+			cte = '%4im%02is' % (cte/60, cte%60)
+			
+		if self.printP:
+			# If we want the percentage also displayed, trim a little 
+			# more from the progress bar's wdith
+			barSpan = self.span - 9
+			nMarks = float(self.amount)/self.max * barSpan
+			nMarksFull = int(nMarks)
+			if nMarksFull < barSpan:
+				partial = nMarks - nMarksFull
+				lastMark = self.rotations[int(partial*len(self.rotations))]
+			else:
+				lastMark = ''
+			bar = self.sym * nMarksFull
+			bar = bar + lastMark
+			bar = bar+(' ' * (barSpan-(nMarksFull+len(lastMark))))
+			nte = "%5.1f%%" % (float(self.amount)/self.max*100)
+			
+			out = "[%s] %s %s" % (bar, nte, cte)
+		else:
+			# Progress bar only
+			barSpan = self.span - 2
+			nMarks = float(self.amount)/self.max * barSpan
+			nMarksFull = int(nMarks)
+			if nMarksFull < barSpan:
+				partial = nMarks - nMarksFull
+				lastMark = self.rotations[int(partial*len(self.rotations))]
+			else:
+				lastMark = ''
+			bar = self.sym * nMarksFull
+			bar = bar + lastMark
+			bar = bar+(' ' * (barSpan-(nMarksFull+len(lastMark))))
+			
+			out = "[%s] %s" % (bar, cte)
+			
+		return out
