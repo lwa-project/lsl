c7f9d1b2ac61c2f61704f1cfcfc0d7f0cf38d55d
Jayce Dowell <jayce.dowell@gmail.com>
2014-10-06 16:27:57 +0000

    Add support for phase centers other than zenith to the lsl.imaging.overlay module.

--- lsl/imaging/overlay.py
+++ lsl/imaging/overlay.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 
 """
-Module that provides a variety of overlays for all-sky images.  These overlays include:
+Module that provides a variety of overlays for all-sky images.  These overlays
+include:
  * the locations and names of sources, 
  * the horizon, 
  * a graticle showing lines of constant RA and dec., and
  * a graticle showing lines of constant azimuth and elevation.
 
-All of the functions in this module accept a matplotlib axes instances that is used for
-plotting.
+All of the functions in this module accept a matplotlib axes instances that 
+is used for plotting.
 
 .. versionadded:: 1.0.1
+
+.. versionchanged:: 1.0.3
+	Added support for overlaying on images with non-zenith phase centers
 """
 
 import aipy
+import ephem
 import numpy
 
 
@@ -23,161 +28,211 @@ __all__ = ["sources", "horizon", "graticleRADec", "graticleAzEl",
 		 "__version__", "__revision__", "__all__"]
 
 
-def sources(ax, aa, srcs, label=True):
+def _radec_of(aa, az, alt):
+	sinDec = numpy.sin(alt)*numpy.sin(aa.lat) + numpy.cos(alt)*numpy.cos(aa.lat)*numpy.cos(az)
+	dec = numpy.arcsin( numpy.clip(sinDec, -1, 1) )
+	cosHA = (numpy.sin(alt) - numpy.sin(dec)*numpy.sin(aa.lat)) / (numpy.cos(dec)*numpy.cos(aa.lat))
+	HA = numpy.arccos( numpy.clip(cosHA, -1, 1) )
+	if numpy.sin(az) > 0:
+		HA = 2*numpy.pi - HA
+	RA = aa.sidereal_time() - HA
+	return RA, dec 
+
+
+def sources(ax, aa, srcs, phaseCenter='z', label=True):
 	"""
 	For a matplotlib axis instance showing an image of the sky, plot the
 	locations of the srcs given in the 'srcs' dictionary.
 	"""
 	
+	# Get the phase center
+	if phaseCenter is not 'z':
+		phaseCenter.compute(aa)
+		pcRA, pcDec = phaseCenter.ra, phaseCenter.dec
+	else:
+		pcRA, pcDec = aa.sidereal_time(), aa.lat
+	rot = aipy.coord.eq2top_m(0, pcDec)
+		
 	# Compute the positions of major sources and label the images
-	compSrc = {}
 	for name,src in srcs.iteritems():
 		src.compute(aa)
-		top = src.get_crds(crdsys='top', ncrd=3)
-		az, alt = aipy.coord.top2azalt(top)
-		compSrc[name] = [az, alt]
-		if alt <= 0:
-			continue
-		ax.plot(top[0], top[1], marker='x', markerfacecolor='None', markeredgecolor='w', 
+		eq = aipy.coord.radec2eq((src.ra-pcRA, src.dec))
+		top = numpy.dot(rot, eq)
+		junk,alt = aipy.coord.top2azalt(top)
+		if alt >= 0:
+			ax.plot(top[0], top[1], marker='x', markerfacecolor='None', markeredgecolor='w', 
 				linewidth=10.0, markersize=10)
-		if label:
-			ax.text(top[0], top[1], name, color='white', size=12)
+			if label:
+				ax.text(top[0], top[1], name, color='white', size=12)
 
 
-def horizon(ax):
+def horizon(ax, aa, phaseCenter='z'):
 	"""
 	For a matplotlib axis instance showing an image of the sky, plot the horizon.
+	
+	.. versionchanged:: 1.0.3
+		Added a new argument for the AntennaArray instance to provide a 
+		uniform default call for all functions.
 	"""
+		
+	# Get the phase center
+	if phaseCenter is not 'z':
+		phaseCenter.compute(aa)
+		pcRA, pcDec = phaseCenter.ra, phaseCenter.dec
+	else:
+		pcRA, pcDec = aa.sidereal_time(), aa.lat
+	rot = aipy.coord.eq2top_m(0, pcDec)
 	
 	# Add in the horizon
-	x = numpy.zeros(361)
-	y = numpy.zeros(361)
+	x = numpy.zeros(361) + numpy.nan
+	y = numpy.zeros(361) + numpy.nan
 	for i in xrange(361):
-		xyz = aipy.coord.azalt2top([i*numpy.pi/180.0, 0])
-		x[i] = xyz[0]
-		y[i] = xyz[1]
+		ra, dec = _radec_of(aa, i*numpy.pi/180.0, 0.0)
+		eq = aipy.coord.radec2eq((ra-pcRA,dec))
+		top = numpy.dot(rot, eq)
+		junk,alt = aipy.coord.top2azalt(top)
+		if alt >= -1e-5:
+			x[i] = top[0]
+			y[i] = top[1]
 	ax.plot(x, y, color='white')
 
 
-def graticleRADec(ax, aa, label=True):
+def graticleRADec(ax, aa, phaseCenter='z', label=True):
 	"""
 	For a matplotlib axis instance showing an image of the sky, plot lines of
 	constant declinate and RA.  Declinations are spaced at 20 degree intervals
 	and RAs are spaced at 2 hour intervals.
 	"""
 	
+	# Get the phase center
+	if phaseCenter is not 'z':
+		phaseCenter.compute(aa)
+		pcRA, pcDec = phaseCenter.ra, phaseCenter.dec
+	else:
+		pcRA, pcDec = aa.sidereal_time(), aa.lat
+	rot = aipy.coord.eq2top_m(0, pcDec)
+	
 	# Lines of constant declination first
 	decs = range(-80, 90, 20)
 	ras = numpy.linspace(0, 360, 800)
 	
-	x = numpy.zeros(ras.size)
-	x = numpy.ma.array(x, mask=numpy.zeros(ras.size))
-	y = numpy.zeros(ras.size)
-	y = numpy.ma.array(y, mask=numpy.zeros(ras.size))
-	
+	x = numpy.zeros(ras.size) + numpy.nan
+	y = numpy.zeros(ras.size) + numpy.nan
 	for dec in decs:
-		x *= 0
-		y *= 0
+		x *= numpy.nan
+		y *= numpy.nan
 		
 		# Loop over RA to compute the topocentric coordinates (used by the image) for
 		# the lines.  Also, figure out the elevation for each point on the line so
 		# we can mask those below the horizon
 		for i,ra in enumerate(ras):
-			eq = aipy.coord.radec2eq((-aa.sidereal_time() + ra*numpy.pi/180,dec*numpy.pi/180))
-			xyz = numpy.dot(aipy.coord.eq2top_m(0, aa.lat), eq)
-			az,alt = aipy.coord.top2azalt(xyz)
-			
-			x[i] = xyz[0]
-			y[i] = xyz[1]
-			if alt <= 0:
-				x.mask[i] = 1
-				y.mask[i] = 1
-			else:
-				x.mask[i] = 0
-				y.mask[i] = 0
+			eq = aipy.coord.radec2eq((ra*numpy.pi/180-pcRA, dec*numpy.pi/180))
+			top = numpy.dot(rot, eq)
+			junk,alt = aipy.coord.top2azalt(top)
+			if alt >= -1e-5:
+				x[i] = top[0]
+				y[i] = top[1]
 				
 		ax.plot(x, y, color='white', alpha=0.75)
 		
-		eq = aipy.coord.radec2eq((-aa.sidereal_time() + aa.sidereal_time(),(dec+5)*numpy.pi/180))
-		xyz = numpy.dot(aipy.coord.eq2top_m(0, aa.lat), eq)
-		az,alt = aipy.coord.top2azalt(xyz)
-		
+		eq = aipy.coord.radec2eq((pcRA-pcRA, (dec+5)*numpy.pi/180))
+		top = numpy.dot(rot, eq)
+		az,alt = aipy.coord.top2azalt(top)
 		if alt > 15*numpy.pi/180 and label:
-			ax.text(xyz[0], xyz[1], '%+i$^\circ$' % dec, color='white')
+			ax.text(top[0], top[1], '%+i$^\circ$' % dec, color='white')
 			
 	# Lines of constant RA			
 	decs = numpy.linspace(-80, 80, 400)
 	ras = range(0,360,30)
 	
-	x = numpy.zeros(decs.size)
-	x = numpy.ma.array(x, mask=numpy.zeros(decs.size))
-	y = numpy.zeros(decs.size)
-	y = numpy.ma.array(y, mask=numpy.zeros(decs.size))
-	
+	x = numpy.zeros(decs.size) + numpy.nan
+	y = numpy.zeros(decs.size) + numpy.nan
 	for ra in ras:
-		x *= 0
-		y *= 0
+		x *= numpy.nan
+		y *= numpy.nan
 		
 		# Loop over dec to compute the topocentric coordinates (used by the image) for
 		# the lines.  Also, figure out the elevation for each point on the line so
 		# we can mask those below the horizon
 		for i,dec in enumerate(decs):
-			eq = aipy.coord.radec2eq((-aa.sidereal_time() + ra*numpy.pi/180,dec*numpy.pi/180))
-			xyz = numpy.dot(aipy.coord.eq2top_m(0, aa.lat), eq)
-			az,alt = aipy.coord.top2azalt(xyz)
-			
-			x[i] = xyz[0]
-			y[i] = xyz[1]
-			if alt <= 0:
-				x.mask[i] = 1
-				y.mask[i] = 1
-			else:
-				x.mask[i] = 0
-				y.mask[i] = 0
+			eq = aipy.coord.radec2eq((ra*numpy.pi/180-pcRA, dec*numpy.pi/180))
+			top = numpy.dot(rot, eq)
+			junk,alt = aipy.coord.top2azalt(top)
+			if alt >= -1e-5:
+				x[i] = top[0]
+				y[i] = top[1]
 				
 		ax.plot(x, y, color='white', alpha=0.75)
 		
-		eq = aipy.coord.radec2eq((-aa.sidereal_time() + ra*numpy.pi/180,0))
-		xyz = numpy.dot(aipy.coord.eq2top_m(0, aa.lat), eq)
-		az,alt = aipy.coord.top2azalt(xyz)
-		
+		eq = aipy.coord.radec2eq((ra*numpy.pi/180-pcRA, 0))
+		top = numpy.dot(rot, eq)
+		az,alt = aipy.coord.top2azalt(top)
 		if alt > 20*numpy.pi/180 and label:
-			ax.text(xyz[0], xyz[1], '%i$^h$' % (ra/15,), color='white')
+			ax.text(top[0], top[1], '%i$^h$' % (ra/15,), color='white')
 
 
-def graticleAzEl(ax, aa, label=True):
+def graticleAzEl(ax, aa, phaseCenter='z', label=True):
 	"""
 	For a matplotlib axis instance showing an image of the sky, plot lines of
 	constant azimuth and elevation.  Elevations are spaced at 20 degree intervals
 	and azimuths are spaced at 45 degree intervals
 	"""
 	
+	# Get the phase center
+	if phaseCenter is not 'z':
+		phaseCenter.compute(aa)
+		pcRA, pcDec = phaseCenter.ra, phaseCenter.dec
+	else:
+		pcRA, pcDec = aa.sidereal_time(), aa.lat
+	rot = aipy.coord.eq2top_m(0, pcDec)
+	
 	# Lines of constant elevation
 	els = range(0, 90, 20)
 	
+	x = numpy.zeros(361) + numpy.nan
+	y = numpy.zeros(361) + numpy.nan
 	for el in els:
-		x = numpy.zeros(361)
-		y = numpy.zeros(361)
+		x *= numpy.nan
+		y *= numpy.nan
+		
 		for i in xrange(361):
-			xyz = aipy.coord.azalt2top([i*numpy.pi/180.0, el*numpy.pi/180.0])
-			x[i] = xyz[0]
-			y[i] = xyz[1]
-			
+			ra, dec = _radec_of(aa, i*numpy.pi/180.0, el*numpy.pi/180.0)
+			eq = aipy.coord.radec2eq((ra-pcRA,dec))
+			top = numpy.dot(rot, eq)
+			junk,alt = aipy.coord.top2azalt(top)
+			if alt >= -1e-5:
+				x[i] = top[0]
+				y[i] = top[1]
+				
 		ax.plot(x, y, color='white')
-		if el > 0:
-			ax.text(x[160], y[160], '%i$^\circ$' % el, color='white')
+		
+		if el > 0 or phaseCenter is not 'z':
+			valid = numpy.where( numpy.isfinite(x) & numpy.isfinite(y) )[0]
+			pos = valid.size / 2 - valid.size / 5
+			if valid.size > 10:
+				ax.text(x[valid[pos]], y[valid[pos]], '%i$^\circ$' % el, color='white')
 			
 	# Lines of constant azimuth
 	azs = range(0, 360, 45)
 	
+	x = numpy.zeros(81) + numpy.nan
+	y = numpy.zeros(81) + numpy.nan
 	for az in azs:
-		x = numpy.zeros(81)
-		y = numpy.zeros(81)
+		x *= numpy.nan
+		y *= numpy.nan
 		
 		for i in xrange(81):
-			xyz = aipy.coord.azalt2top([az*numpy.pi/180.0, i*numpy.pi/180.0])
-			x[i] = xyz[0]
-			y[i] = xyz[1]
-			
+			ra, dec = _radec_of(aa, az*numpy.pi/180.0, i*numpy.pi/180.0)
+			eq = aipy.coord.radec2eq((ra-pcRA,dec))
+			top = numpy.dot(rot, eq)
+			junk,alt = aipy.coord.top2azalt(top)
+			if alt >= -1e-5:
+				x[i] = top[0]
+				y[i] = top[1]
+				
 		ax.plot(x, y, color='white')
-		ax.text(x[45], y[45], '%i$^\circ$' % az, color='white')
+		
+		valid = numpy.where( numpy.isfinite(x) & numpy.isfinite(y) )[0]
+		pos = valid.size / 2 - valid.size / 5
+		if valid.size > 10:
+			ax.text(x[valid[pos]], y[valid[pos]], '%i$^\circ$' % az, color='white')
