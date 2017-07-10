9f354e6512a7a1389db21406cf74c7a0bab8a20c
Jayce Dowell <jayce.dowell@gmail.com>
2017-07-10 19:20:22 +0000

    Message removed a few over BSD-specific things following r2101.

--- lsl/reader/drspec.c
+++ lsl/reader/drspec.c
@@ -13,12 +13,6 @@
 #define __bswap_16 OSSwapInt16
 #define __bswap_32 OSSwapInt32
 #define __bswap_64 OSSwapInt64
-#else
-/* BSD */
-#include <sys/endian.h>
-#define __bswap_16 __bswap16_var
-#define __bswap_32 __bswap32_var
-#define __bswap_64 __bswap64_var
 #endif
 
 #define NO_IMPORT_ARRAY
