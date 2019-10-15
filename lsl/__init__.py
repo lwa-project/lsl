71e0026978d97bc3b0bbd2fc8f28deec15109cf3
Jayce Dowell <jayce.dowell@gmail.com>
2019-10-15 15:40:37 +0000

    Worked on adding back in the on-demand LSL test suite.

--- lsl/__init__.py
+++ lsl/__init__.py
@@ -28,8 +28,23 @@ import sys
 if sys.version_info > (3,):
     xrange = range
     
+import os
+import subprocess
+
 from lsl import version
+from lsl.common import paths
 
 __version__ = '0.8'
 __revision__ = '$Rev$'
 __author__ = "Jayce Dowell"
+
+
+def test(): 
+    """ 
+    Internal test fixture 
+    """ 
+    
+    eggPath = os.path.split(paths.MODULE)[0] 
+    testPath = os.path.join(eggPath, 'tests', 'test_lsl.py') 
+    subprocess.check_call([sys.executable, testPath, '-v'])
+
