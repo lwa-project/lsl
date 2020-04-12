c5f5a0e07d4aa8187b13e7133137eed954861272
jaycedowell <jayce.dowell@gmail.com>
2020-04-12 07:19:07 -0600

    I'm not sure if there is a good way to re-implement ephem.Equatorial, ephem.Ecliptic, or ephem.Galactic using astropy.

--- lsl/_astroephem/coordinates.py
+++ lsl/_astroephem/coordinates.py
@@ -1,15 +1,18 @@
 from __future__ import print_function, division
 
 from astropy.time import Time
-from astropy.coordinates import ICRS, GeocentricTrueEcliptic, Galactic as AstroGalactic
+from astropy.coordinates import ICRS, ITRS, FK4, FK5, GeocentricTrueEcliptic, Galactic as AstroGalactic
 import astropy.units as u
 
 from angles import hours, degrees
-from dates import Date
+from dates import Date, B1950, J2000
 
 __all__ = ['Equatorial', 'Ecliptic', 'Galactic']
 
+# I'm not sure how to really do this
+from ephem import Equatorial, Ecliptic, Galactic
 
+"""
 class _Coordinate(object):
     __astropy_base__ = ICRS
     
@@ -21,9 +24,13 @@ class _Coordinate(object):
         
         if isinstance(c1_or_class, _Coordinate):
             try:
-                new_frame = self.__astropy_base__(obstime=self._epoch)
+                new_frame = self.__astropy_base__(equinox=self._epoch,
+                                                  obstime=self._epoch)
             except TypeError:
-                new_frame = self.__astropy_base__()
+                try:
+                    new_frame = self.__astropy_base__(obstime=self._epoch)
+                except TypeError:
+                    new_frame = self.__astropy_base__()
             self._coord = c1_or_class._coord.transform_to(new_frame)
         else:
             assert(c2 is not None)
@@ -47,16 +54,24 @@ class _Coordinate(object):
         c1 = hours(c1) if self._hours else degrees(c1)
         c2 = degrees(c2)
         try:
-            self._coord = self.__astropy_base__(c1, c2, obstime=self._epoch)
+            self._coord = self.__astropy_base__(c1, c2, equinox=self._epoch,
+                                                        obstime=self._epoch)
         except TypeError:
-            self._coord = self.__astropy_base__(c1, c2)
-            
+            try:
+                self._coord = self.__astropy_base__(c1, c2, obstime=self._epoch)
+            except TypeError:
+                self._coord = self.__astropy_base__(c1, c2)
+                
     def from_radec(self, ra, dec):
         radec = ICRS(hours(ra), degres(dec))
         try:
-            new_frame = self.__astropy_base__(obstime=self._epoch)
+            new_frame = self.__astropy_base__(equinox=self._epoch,
+                                              obstime=self._epoch)
         except TypeError:
-            new_frame = self.__astropy_base__()
+            try:
+                new_frame = self.__astropy_base__(obstime=self._epoch)
+            except TypeError:
+                new_frame = self.__astropy_base__()
         self._coord = c1_or_class.transform_to(new_frame)
                
 
@@ -64,15 +79,22 @@ class Equatorial(_Coordinate):
     __astropy_base__ = ICRS
     
     def __init__(self, ra_or_class, dec=None, epoch=None):
+        if epoch == B1950:
+            __astropy_base__ = FK4
+        elif epoch == J2000:
+            __astropy_base__ = FK5
+            
         _Coordinate.__init__(self, ra_or_class, dec, epoch=epoch, hours=True)
         
     @property
     def ra(self):
-        return self._coord.ra.to('radian').value
+        _gc = self._coord.transform_to(ITRS(obstime=self._epoch))
+        return _gc.ra.to('radian').value
         
     @property
     def dec(self):
-        return self._coord.dec.to('radian').value
+        _gc = self._coord.transform_to(ITRS(obstime=self._epoch))
+        return _gc.dec.to('radian').value
 
 
 class Ecliptic(_Coordinate):
@@ -103,3 +125,4 @@ class Galactic(_Coordinate):
     @property
     def lat(self):
         return self._coord.b.to('radian').value
+"""
