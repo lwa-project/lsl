62900c2b703c71a2e0ebc78c2746181b69e17ad4
jaycedowell <jayce.dowell@gmail.com>
2020-05-05 19:30:46 -0600

    Yet another attempt at getting compresses ms files tested.

--- tests/test_imaging.py
+++ tests/test_imaging.py
@@ -354,9 +354,9 @@ class imaging_tests(unittest.TestCase):
         
         # Compress
         compressedFile = os.path.splitext(testFile)[0]+'.tar.gz'
-        cmd = ['tar', '-C', testFile,  'czf', compressedFile]
-        cmd.extend([filename.replace('testFile', './') for filename in glob.glob(os.path.join(testFile, '*'))])
-        subprocess.check_call(cmd)
+        cmd = ['tar', 'czf', compressedFile]
+        cmd.extend([filename.replace(testFile, './') for filename in glob.glob(os.path.join(testFile, '*'))])
+        subprocess.check_call(cmd, cwd=testFile)
         
         # Open the measurement set
         ms = utils.CorrelatedDataMS(compressedFile)
