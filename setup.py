9591aa24a4fa042009f622d6174b2329bdc29ce2
jaycedowell <jayce.dowell@gmail.com>
2020-08-12 09:37:56 -0600

    Does this work now?

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ PY2 = sys.version_info.major < 3
 if PY2:
     ASTROPY_VERSION = 'astropy<3.0'
 else:
-    ASTROPY_VERSION = 'astropy>=3.0,<3.2'
+    ASTROPY_VERSION = 'astropy>=3.0'#,<3.2'
 
 
 def get_version():
