c4fb22e042604e2dbf36c53062484fbc66219792
jaycedowell <jayce.dowell@gmail.com>
2021-04-07 09:54:01 -0600

    I think this is better.

--- lsl/_astroephem/coordinates.py
+++ lsl/_astroephem/coordinates.py
@@ -1,7 +1,11 @@
+"""
+Python module providing ephem-like conversions between coordinate systems.
+"""
+
 from __future__ import print_function, division
 
 from astropy.time import Time
-from astropy.coordinates import ICRS, ITRS, FK4, FK5, GeocentricTrueEcliptic, Galactic as AstroGalactic
+from astropy.coordinates import ICRS, ITRS, CIRS, FK4, FK5, GeocentricTrueEcliptic, Galactic as AstroGalactic
 import astropy.units as u
 
 from angles import hours, degrees
@@ -9,11 +13,12 @@ from dates import Date, B1950, J2000
 
 __all__ = ['Equatorial', 'Ecliptic', 'Galactic']
 
-# I'm not sure how to really do this
-from ephem import Equatorial, Ecliptic, Galactic
 
-"""
-class _Coordinate(object):
+class EphemCoordinate(object):
+    """
+    Base class to facilitate the coordinate transforms.
+    """
+    
     __astropy_base__ = ICRS
     
     def __init__(self, c1_or_class, c2=None, epoch=None, hours=False):
@@ -22,7 +27,7 @@ class _Coordinate(object):
         self._epoch = epoch
         self._hours = hours
         
-        if isinstance(c1_or_class, _Coordinate):
+        if isinstance(c1_or_class, EphemCoordinate):
             try:
                 new_frame = self.__astropy_base__(equinox=self._epoch,
                                                   obstime=self._epoch)
@@ -37,20 +42,36 @@ class _Coordinate(object):
             self.set(c1_or_class, c2)
             
     def get(self):
+        """
+        Get the coordinate pair as two-element tuple of EphemAngle instances.
+        Exactly what is returned is dependent on the frame used for the
+        instance.
+        """
+        
         try:
-            c1, c2 = self._coord.lon, self._coord.lat
+            c1, c2 = degrees(self._coord.lon), degrees(self._coord.lat)
         except AttributeError:
             try:
-                c1, c2 = self._coord.l, self._coord.b
+                c1, c2 = degrees(self._coord.l), degrees(self._coord.b)
             except AttributeError:
-                c1, c2 = self._coord.ra, self._coord.dec
+                c1 = hours(self._coord.ra) if self._hours else degrees(self._coord.ra)
+                c2 = degrees(self._coord.dec)
         return c1, c2
-      
+        
     def to_radec(self):
+        """
+        Return the ICRS right assencsion and declination for the coordinate.
+        """
+        
         radec = self._coord.transform_to(ICRS())
-        return (radec.ra, radec.dec)
+        return (hours(radec.ra), degrees(radec.dec))
         
     def set(self, c1, c2):
+        """
+        Set the coordinates for the instance.  Exactly what is set is dependent
+        on the frame used for the instance.
+        """
+        
         c1 = hours(c1) if self._hours else degrees(c1)
         c2 = degrees(c2)
         try:
@@ -63,6 +84,11 @@ class _Coordinate(object):
                 self._coord = self.__astropy_base__(c1, c2)
                 
     def from_radec(self, ra, dec):
+        """
+        Set the coordinates for the instance using the provided ICRS right
+        ascension and declination.
+        """
+        
         radec = ICRS(hours(ra), degres(dec))
         try:
             new_frame = self.__astropy_base__(equinox=self._epoch,
@@ -75,54 +101,91 @@ class _Coordinate(object):
         self._coord = c1_or_class.transform_to(new_frame)
                
 
-class Equatorial(_Coordinate):
+class Equatorial(EphemCoordinate):
+    """
+    Class for representing an Equatorial position on the sky.  The default frame
+    is ICRS.  Setting an epoch of J2000 sets the frame to FK5 and setting it to
+    B1950 set the frame to FK4.
+    """
+    
     __astropy_base__ = ICRS
     
     def __init__(self, ra_or_class, dec=None, epoch=None):
         if epoch == B1950:
-            __astropy_base__ = FK4
+            self.__astropy_base__ = FK4
         elif epoch == J2000:
-            __astropy_base__ = FK5
+            self.__astropy_base__ = FK5
             
-        _Coordinate.__init__(self, ra_or_class, dec, epoch=epoch, hours=True)
+        EphemCoordinate.__init__(self, ra_or_class, dec, epoch=epoch, hours=True)
         
     @property
     def ra(self):
-        _gc = self._coord.transform_to(ITRS(obstime=self._epoch))
-        return _gc.ra.to('radian').value
+        """
+        Right ascension in the current frame.
+        """
+        
+        return hours(self._coord.ra)
         
     @property
     def dec(self):
-        _gc = self._coord.transform_to(ITRS(obstime=self._epoch))
-        return _gc.dec.to('radian').value
+        """
+        Declination in the current frame.
+        """
+        
+        return degrees(self._coord.dec)
 
 
-class Ecliptic(_Coordinate):
+class Ecliptic(EphemCoordinate):
+    """
+    Class for representing an Ecliptic position on the sky in the geocentric true
+    ecliptic frame.
+    """
+    
     __astropy_base__ = GeocentricTrueEcliptic
     
     def __init__(self, lon_or_class, lat=None, epoch=None):
-        _Coordinate.__init__(self, lon_or_class, lat, epoch=epoch, hours=False)
+        EphemCoordinate.__init__(self, lon_or_class, lat, epoch=epoch, hours=False)
         
     @property
     def lon(self):
-        return self._coord.lon.to('radian').value
+        """
+        Ecliptic longitude.
+        """
+        
+        return degrees(self._coord.lon)
         
     @property
     def lat(self):
-        return self._coord.lat.to('radian').value
+        """
+        Ecliptic latitude.
+        """
+        
+        return degrees(self._coord.lat)
 
 
-class Galactic(_Coordinate):
+class Galactic(EphemCoordinate):
+    """
+    Class for representing a Galactic position on the sky in the geocentric true
+    ecliptic frame.
+    """
+    
     __astropy_base__ = AstroGalactic
     
     def __init__(self, lon_or_class, lat=None, epoch=None):
-        _Coordinate.__init__(self, lon_or_class, lat, epoch=epoch, hours=False)
+        EphemCoordinate.__init__(self, lon_or_class, lat, epoch=epoch, hours=False)
         
     @property
     def lon(self):
-        return self._coord.l.to('radian').value
+        """
+        Galactic longitude.
+        """
+        
+        return degrees(self._coord.l)
         
     @property
     def lat(self):
-        return self._coord.b.to('radian').value
-"""
+        """
+        Galactic latitude.
+        """
+        
+        return degrees(self._coord.b)
