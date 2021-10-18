3403d293dda47ffdbe67770597507dcad251dcda
jaycedowell <jaycedowell@gmail.com>
2021-10-18 10:45:49 -0600

    Another approach to this AIPY problem.

--- lsl/imaging/data.py
+++ lsl/imaging/data.py
@@ -13,6 +13,7 @@ import copy
 import numpy
 
 from lsl import astro
+from lsl.sim.vis import AntennaArray
 
 from lsl.misc import telemetry
 telemetry.track_module()
@@ -144,8 +145,11 @@ class VisibilityDataSet(object):
         self.uvw = uvw
         if antennaarray is not None and not isinstance(antennaarray, aipy.amp.AntennaArray):
             raise TypeError("Expected antennaarray to be either None or AntennaArray")
-        self.antennaarray = copy.deepcopy(antennaarray)
-        if self.antennaarray is not None:
+        if self.antennaarray is None:
+            self.antennaarray = None
+        else:
+            self.antennaarray = AntennaArray((antennaarray.lat, antennaarray.long, antennaarray.elev),
+                                             copy.deepcopy(antennaarray.ants))
             self.antennaarray.set_jultime(self.jd)
         self.phase_center = phase_center
         
