55cdeedcde5bb5fa1691ed23ebfa928a940a0215
jaycedowell <jayce.dowell@gmail.com>
2021-12-13 19:55:13 -0700

    Revert "Yet another catch for the special case of _data_ci8."
    This reverts commit 13a2de6b086d45c95d8a9d74a9960c94ad467029.

--- lsl/sim/drx.py
+++ lsl/sim/drx.py
@@ -146,11 +146,8 @@ class SimFrame(drx.Frame):
         self.payload.timetag = self.obs_time
         self.payload.flags = self.flags
         self.payload._data = self.data
-        try:
-            del self.payload._data_ci8
-        except AttributeError:
-            pass
-            
+        del self.payload._data_ci8
+        
     def load_frame(self, drx_frame):
         """
         Populate the a drx.SimFrame object with a pre-made frame.
