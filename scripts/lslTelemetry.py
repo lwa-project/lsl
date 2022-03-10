96ea2493c9bab483ae1124da7d3b0ae02b527440
jaycedowell <jaycedowell@gmail.com>
2022-03-10 15:38:33 -0700

    Allow 'python -m lsl.misc.telemetry' to control telemetry.

--- scripts/lslTelemetry.py
+++ scripts/lslTelemetry.py
@@ -18,33 +18,5 @@ from lsl.misc import telemetry
 telemetry.track_script()
 
 
-def main(args):
-    # Toggle
-    if args.enable:
-        telemetry.enable()
-    elif args.disable:
-        telemetry.disable()
-        
-    # Report
-    ## Status
-    print("LSL Telemetry is %s" % ('active' if telemetry.is_active() else 'in-active'))
-    
-    ## Key
-    if args.key:
-        print("  Identification key: %s" % telemetry._INSTALL_KEY)
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-            description='update the LSL telemetry setting', 
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter
-            )
-    tgroup = parser.add_mutually_exclusive_group(required=False)
-    tgroup.add_argument('-e', '--enable', action='store_true', 
-                        help='enable telemetry for LSL')
-    tgroup.add_argument('-d', '--disable', action='store_true', 
-                        help='disable telemetry for LSL')
-    parser.add_argument('-k', '--key', action='store_true',
-                        help='show install identification key')
-    args = parser.parse_args()
-    main(args)
+    os.system("%s -m lsl.misc.telemetry %s" % (sys.executable, " ".join(sys.argv[1:])))
