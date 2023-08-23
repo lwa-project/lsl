2bb8620ac25b68fa908c8db223ca83cb922229ce
jaycedowell <jayce.dowell@gmail.com>
2023-08-23 13:56:41 -0600

    Update to match MCS.

--- lsl/common/mcsNDP.py
+++ lsl/common/mcsNDP.py
@@ -72,29 +72,29 @@ __all__ = ['ME_SSMIF_FORMAT_VERSION', 'ME_MAX_NSTD', 'ME_MAX_NFEE', 'ME_MAX_FEEI
 
 
 ME_SSMIF_FORMAT_VERSION = 10	# SSMIF format version code
-ME_MAX_NSTD = 64			# Maximum number of stands that can be described
-ME_MAX_NFEE = 64			# Maximum number of FEEs that can be described
+ME_MAX_NSTD = 256			# Maximum number of stands that can be described
+ME_MAX_NFEE = 256			# Maximum number of FEEs that can be described
 ME_MAX_FEEID_LENGTH = 10		# Number of characters in FEE ID name
 ME_MAX_RACK = 6			# Maximum number of racks?
 ME_MAX_PORT = 50			# Maximum number of ports?
-ME_MAX_NRPD = 128			# Maxmimum number of RPD cables
+ME_MAX_NRPD = 512			# Maxmimum number of RPD cables
 ME_MAX_RPDID_LENGTH = 25		# Number of characters in the RPD ID name
-ME_MAX_NSEP = 128			# Maximum number of SEP cable connections
+ME_MAX_NSEP = 512			# Maximum number of SEP cable connections
 ME_MAX_SEPID_LENGTH = 25		# Number of characters in the SEP ID name
 ME_MAX_SEPCABL_LENGTH = 25	# Number of characters in the SEP cable ID name
-ME_MAX_NARB = 8			# Maximum number of ARX boards
+ME_MAX_NARB = 32			# Maximum number of ARX boards
 ME_MAX_NARBCH = 16			# Number of ARX channels per board
 ME_MAX_ARBID_LENGTH = 10		# Number of characters in the ARX ID name
-ME_MAX_NSNAP = 2			# Maximum number of SNAP boards
+ME_MAX_NSNAP = 8			# Maximum number of SNAP boards
 ME_MAX_NSNAPCH = 64		# Number of channels per SNAP board
 ME_MAX_SNAPID_LENGTH = 10	# Number of characters in the SNAP board ID name
-ME_MAX_NSERVER = 3			# Maximum number of server
+ME_MAX_NSERVER = 9			# Maximum number of server
 ME_MAX_SERVERID_LENGTH = 10	# Number of characters in the server ID name
 ME_MAX_NDR = 4				# Maximum number of data recorders
 ME_MAX_DRID_LENGTH = 10		# Number of characters in the DR ID name
 ME_MAX_NPWRPORT = 50		# Maximum number of power ports
 ME_MAX_SSNAME_LENGTH = 3		# Number of characters in the power port ID names, for codes used for PWR_NAME
-LWA_MAX_NSTD = 64			# Maximum number of stands for the LWA
+LWA_MAX_NSTD = 256			# Maximum number of stands for the LWA
 TPSS_FORMAT_VERSION = 6		# MCS0030 format version code
 
 
