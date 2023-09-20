4c395cd77a493559654ea2386185f74e287deabc
Craig Taylor <73905895+ctaylor-physics@users.noreply.github.com>
2023-09-20 15:06:59 -0600

    Update imageIDI.py; epic update
    Redesign of input formatting for imageIDI. Now one would easily use it to create images with image plane inputs rather than aperture plane.

--- scripts/imageIDI.py
+++ scripts/imageIDI.py
@@ -35,8 +35,6 @@ MST = pytz.timezone('US/Mountain')
 UTC = pytz.UTC
 
 
-NPIX_SIDE = 350
-
 
 def main(args):
     filename = args.filename
@@ -48,6 +46,11 @@ def main(args):
     nStand = len(idi.stands)
     nchan = len(idi.freq)
     freq = idi.freq
+
+    NPIX_SIDE = args.npix_side
+    image_resolution = args.image_res
+    res = (2 * NPIX_SIDE * numpy.sin(numpy.pi * image_resolution / 360 ))**-1
+    uv_size = NPIX_SIDE * res
     
     print("Raw Stand Count: %i" % nStand)
     print("Final Baseline Count: %i" % (nStand*(nStand-1)/2,))
@@ -105,7 +108,7 @@ def main(args):
         lbl1 = 'XX'
         for p in ('XX', 'RR', 'I'):
             try:
-                img1 = utils.build_gridded_image(dataDict, size=NPIX_SIDE//2, res=0.5, pol=p, chan=toWork)
+                img1 = utils.build_gridded_image(dataDict, size=NPIX_SIDE//2, res=res, pol=p, uv_size=uv_size, chan=toWork)
                 lbl1 = p.upper()
             except:
                 pass
@@ -114,7 +117,7 @@ def main(args):
         lbl2 = 'YY'
         for p in ('YY', 'LL', 'Q'):
             try:
-                img2 = utils.build_gridded_image(dataDict, size=NPIX_SIDE//2, res=0.5, pol=p, chan=toWork)
+                img2 = utils.build_gridded_image(dataDict, size=NPIX_SIDE//2, res=res, pol=p, uv_size=uv_size, chan=toWork)
                 lbl2 = p.upper()
             except:
                 pass
@@ -123,7 +126,7 @@ def main(args):
         lbl3 = 'XY'
         for p in ('XY', 'RL', 'U'):
             try:
-                img3 = utils.build_gridded_image(dataDict, size=NPIX_SIDE//2, res=0.5, pol=p, chan=toWork)
+                img3 = utils.build_gridded_image(dataDict, size=NPIX_SIDE//2, res=res, pol=p, uv_size=uv_size, chan=toWork)
                 lbl3 = p.upper()
             except:
                 pass
@@ -132,7 +135,7 @@ def main(args):
         lbl4 = 'YX'
         for p in ('YX', 'LR', 'V'):
             try:
-                img4 = utils.build_gridded_image(dataDict, size=NPIX_SIDE//2, res=0.5, pol=p, chan=toWork)
+                img4 = utils.build_gridded_image(dataDict, size=NPIX_SIDE//2, res=res, pol=p, uv_size=uv_size, chan=toWork)
                 lbl4 = p.upper()
             except:
                 pass
@@ -194,6 +197,23 @@ def main(args):
         plt.show()
         
         if args.fits is not None:
+            ## Make Zenith PC so SV can overwrite it
+            zra = lo.sidereal_time()*180/numpy.pi
+            zdec = lo.lat*180/numpy.pi
+
+            ## Create Phase Pointing in RA/DEC for LWA-SV if requested
+            if args.lwasv:
+                obstime = astropy.time.Time(jdList[0], format='jd')
+                loc = EarthLocation(lat=34.3484, lon=-106.8858, height=1477.8*u.m) #Approximates LWA-SV location
+                z_skycoord = SkyCoord(AltAz(az = 90.9743763*u.deg, alt = 88.0666283*u.deg, obstime = obstime, location = loc))
+                zra, zdec = z_skycoord.transform_to('icrs').ra, z_skycoord.transform_to('icrs').dec
+                zra = zra.deg
+                zdec = zdec.deg
+
+            
+            ### Make ImgWPlus object for proper pixel scale
+            fits_imgwp = utils.ImgWPlus(size=uv_size,res=res)
+        
             ## Loop over the images to build up the FITS file
             hdulist = [astrofits.PrimaryHDU(),]
             for img,pol in zip((img1,img2,img3,img4), (lbl1,lbl2,lbl3,lbl4)):
@@ -210,12 +230,12 @@ def main(args):
                 hdu.header['EPOCH'] = 2000.0 + (jdList[0] - 2451545.0) / 365.25
                 hdu.header['CTYPE1'] = 'RA---SIN'
                 hdu.header['CRPIX1'] = img.shape[0]//2+1
-                hdu.header['CDELT1'] = -360.0/img.shape[0]/numpy.pi
-                hdu.header['CRVAL1'] = lo.sidereal_time()*180/numpy.pi	# pylint:disable=no-member
+                hdu.header['CDELT1'] = -1 * np.degrees(fits_imgwp.pixel_size)
+                hdu.header['CRVAL1'] = zra
                 hdu.header['CTYPE2'] = 'DEC--SIN'
                 hdu.header['CRPIX2'] = img.shape[1]//2+1
-                hdu.header['CDELT2'] = 360.0/img.shape[1]/numpy.pi
-                hdu.header['CRVAL2'] = lo.lat*180/numpy.pi
+                hdu.header['CDELT2'] = np.degrees(fits_imgwp.pixel_size)
+                hdu.header['CRVAL2'] = zdec
                 hdu.header['LONPOLE'] = 180.0
                 hdu.header['LATPOLE'] = 90.0
                 
@@ -249,6 +269,10 @@ if __name__ == "__main__":
                         help='first frequency to analyze in MHz')
     parser.add_argument('-2', '--freq-stop', type=aph.frequency, default='88.0', 
                         help='last frequency to analyze in MHz')
+    parser.add_argument('-p', '--npix-side', type=int, default=350,
+                        help='number of pixels per side in image plane')
+    parser.add_argument('-r', '--image-res', type=float, default=0.37142,
+                        help='resolution in the image plane as degrees/pix')
     parser.add_argument('-s', '--dataset', type=int, default=-1, 
                         help='data set to image')
     parser.add_argument('-m', '--uv-min', type=float, default=0.0, 
@@ -265,6 +289,8 @@ if __name__ == "__main__":
                         help='disable source and grid labels')
     parser.add_argument('-g', '--no-grid', action='store_true', 
                         help='disable the coordinate grid')
+    parser.add_argument('-v', '--lwasv', action='store_true',
+                        help='set phase center to report correctly for LWA-SV')
     parser.add_argument('-f', '--fits', type=str, 
                         help='save the images to the specified FITS image file')
     args = parser.parse_args()
