f283079581c32a3b9ad7fd14e7a139558a6c8e09
jaycedowell <jayce.dowell@gmail.com>
2023-10-31 11:51:04 -0600

    It begins...

--- lsl/catalog.py
+++ lsl/catalog.py
@@ -2,12 +2,6 @@
 LWA astronomical source catalogs.
 """
 
-# Python2 compatibility
-from __future__ import print_function, division, absolute_import
-import sys
-if sys.version_info < (3,):
-    range = xrange
-    
 import os
 import math
 import abc
