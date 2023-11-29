b04e5d25d908e75bf286acbb95e2eedad510c5c5
jaycedowell <jayce.dowell@gmail.com>
2023-11-29 13:15:15 -0700

    Work on a new module to help with beam models.

---
+++ lsl/sim/beam.py
@@ -0,0 +1,306 @@
+import os
+import h5py
+import numpy as np
+from scipy.interpolate import interp1d, RegularGridInterpolator
+
+from lsl.common.paths import DATA as dataPath
+
+
+_EMP_FILENAME  = os.path.join(dataPath, 'lwa1-dipole-emp.npz')
+_NEC_FILENAME  = 'test.hdf5'
+_FEKO_FILENAME = '/Users/jaycedowell/Downloads/OVRO_LWA-FEKO_er-3p5_sig-0p002.h5'
+_CST_FILENAME  = '/Users/jaycedowell/Downloads/OVRO_LWA-CST_er-3p5_sig-0p002.h5'
+
+
+def _load_response_fitted(frequency, corrected=False):
+    """
+    Given an observing frequency in Hz, load in the empirical model (see LWA
+    memos #175 and #178) for an isolated LWA dipole and compute its response at
+    a collection of azimuths and altiudes.  The `corrected` keyword enables an 
+    altitude-based correction to the model based on work by Dowell et al. (2017).
+    Returns a seven-element list of:
+     * frequencies represented (1D; Hz)
+     * azimuths computed (1D; degrees)
+     * altitudes computed (1D; degrees)
+     * X pol. E plane response (3D; freq x alt x az; normalized)
+     * X pol. H plane response (3D; freq x alt x az; normalized)
+     * Y pol. E plane response (3D; freq x alt x az; normalized)
+     * Y pol. H plane response (3D; freq x alt x az; normalized)
+     
+    .. note:: This empirical model is only valid for XX and YY and the returned
+              values the E and H plane responses are sqrt(XX/2) or sqrt(YY/2).
+    """
+    
+    for pol in ('X', 'Y'):
+        # Get the empirical model of the beam and compute it for the correct frequencies
+        beamDict = np.load(_EMP_FILENAME)
+        beamCoeff = beamDict[f"fit{pol.upper()}"]
+        alphaE = np.polyval(beamCoeff[0,0,:], frequency)
+        betaE =  np.polyval(beamCoeff[0,1,:], frequency)
+        gammaE = np.polyval(beamCoeff[0,2,:], frequency)
+        deltaE = np.polyval(beamCoeff[0,3,:], frequency)
+        alphaH = np.polyval(beamCoeff[1,0,:], frequency)
+        betaH =  np.polyval(beamCoeff[1,1,:], frequency)
+        gammaH = np.polyval(beamCoeff[1,2,:], frequency)
+        deltaH = np.polyval(beamCoeff[1,3,:], frequency)
+        
+        if corrected:
+            corrDict = np.load(os.path.join(dataPath, 'lwa1-dipole-cor.npz'))
+            cFreqs = corrDict['freqs']
+            cAlts  = corrDict['alts']
+            if corrDict['degrees'].item():
+                cAlts *= np.pi / 180.0
+            cCorrs = corrDict['corrs']
+            corrDict.close()
+            
+            if frequency/1e6 < cFreqs.min() or frequency/1e6 > cFreqs.max():
+                print("WARNING: Input frequency of %.3f MHz is out of range, skipping correction" % (frequency/1e6,))
+                corrFnc = None
+            else:
+                fCors = cAlts*0.0
+                for j in range(fCors.size):
+                    ffnc = interp1d(cFreqs, cCorrs[:,j], bounds_error=False)
+                    fCors[j] = ffnc(frequency/1e6)
+                corrFnc = interp1d(cAlts, fCors, bounds_error=False)
+                
+        else:
+            corrFnc = None
+            
+        def compute_beam_pattern(az, alt, corr=corrFnc):
+            zaR = np.pi/2 - alt*np.pi / 180.0 
+            azR = az*np.pi / 180.0
+            
+            c = 1.0
+            if corrFnc is not None:
+                c = corrFnc(alt*np.pi / 180.0)
+                c = np.where(np.isfinite(c), c, 1.0)
+                
+            pE = (1-(2*zaR/np.pi)**alphaE)*np.cos(zaR)**betaE + gammaE*(2*zaR/np.pi)*np.cos(zaR)**deltaE
+            pH = (1-(2*zaR/np.pi)**alphaH)*np.cos(zaR)**betaH + gammaH*(2*zaR/np.pi)*np.cos(zaR)**deltaH
+
+            return c*np.sqrt((pE*np.cos(azR))**2 + (pH*np.sin(azR))**2)
+
+        # Calculate the beam
+        az = np.arange(361)
+        alt = np.arange(91)
+        maz, malt = np.meshgrid(az, alt)
+        resp = np.sqrt(compute_beam_pattern(maz, malt)/2)
+        resp.shape = (1,)+resp.shape
+        
+        try:
+            output.extend([resp, resp])
+        except NameError:
+            output = [np.array([frequency]), az, alt, resp, resp]
+            
+    return output
+
+
+def _load_response_full(frequency, model='feko'):
+    """
+    Given an observing frequency in Hz, load in data from an electromagnetic
+    model for an isolated LWA dipole as a function of frequency, azimuth, and
+    altitude.  Returns a seven-element list of:
+     * frequencies represented (1D; Hz)
+     * azimuths computed (1D; degrees)
+     * altitudes computed (1D; degrees)
+     * X pol. E plane response (3D; freq x alt x az; normalized)
+     * X pol. H plane response (3D; freq x alt x az; normalized)
+     * Y pol. E plane response (3D; freq x alt x az; normalized)
+     * Y pol. H plane response (3D; freq x alt x az; normalized)
+    """
+    
+    model = model.lower()
+    if model == 'nec':
+        filename = _NEC_FILENAME
+    elif model == 'feko':
+        filename = _FEKO_FILENAME
+    elif model == 'cst':
+        filename = _CST_FILENAME
+    else:
+        raise ValueError(f"Unknown dipole model source '{model}'")
+        
+    for pol in ('X', 'Y'):
+        if model == 'feko':
+            ## Apparent polarization swap
+            pol = {'X': 'Y', 'Y': 'X'}[pol]
+            
+        # Open the file and select the mode relevant frequencies
+        h = h5py.File(filename)
+        mfreq = h['Freq(Hz)'][...]*1e6
+        best = np.where(np.abs(mfreq - frequency) < 10e6)[0]
+        maz = h['phi_pts'][...]
+        malt = 90 - h['theta_pts'][...] # zenith angle -> elevation
+        mflds = h[f"{pol.upper()}-pol_Efields"]
+        E = mflds['Etheta(Mag)'][best,:,:maz.size]*np.exp(1j*mflds['Etheta(Phase)'][best,:,:maz.size])
+        H = mflds['Ephi(Mag)'][best,:,:maz.size]*np.exp(1j*mflds['Ephi(Phase)'][best,:,:maz.size])
+        s = max([E.max(), H.max()])
+        E /= s
+        H /= s
+        
+        if maz.max() == 359:
+            ## Catch to make sure we can wrap around the north
+            maz = np.append(maz, 360)
+            E = np.append(E, E[...,[0,]], axis=2)
+            H = np.append(H, H[...,[0,]], axis=2)
+            
+        try:
+            output.extend([E,H])
+        except NameError:
+            output = [mfreq[best], maz, malt, E, H]
+            
+    return output
+
+
+def _jones_matrix(model, frequency=74e6):
+    """
+    Given a EM model name, load in the model and return a four element tuple of:
+     * frequencyy (1D; Hz)
+     * azimuth (1D; degrees)
+     * altitude (1D; degrees)
+     * Jones matrix (5D; 2 x 2 x freq x alt x az)
+    """
+    
+    # Load
+    model = model.lower()
+    if model == 'empirical':
+        mfreq, maz, malt, XE, XH, YE, YH = _load_response_fitted(frequency, corrected=False)
+    elif model == 'llfss':
+        mfreq, maz, malt, XE, XH, YE, YH = _load_response_fitted(frequency, corrected=True)
+    else:
+        mfreq, maz, malt, XE, XH, YE, YH = _load_response_full(frequency, model=model)
+        
+    # Create the Jones matrix
+    J = np.array([[XE, XH],
+                  [YE, YH]])
+                  
+    # Return
+    return mfreq, maz, malt, J
+
+
+def mueller_matrix(model, az, alt, frequency=74e6, degrees=True):
+    """
+    Given a model source, an array of azimuth values and an array of altitude
+    values, compute the Mueller matrix for an isolated LWA dipole.  Returns the
+    Mueller matrix with shape (4,4)+az.shape.
+    """
+    
+    # Convert az/alt if needed
+    if not degrees:
+        az = az * 180/np.pi
+        alt = alt * 180/np.pi
+        
+    # Load in correct the Jones matrix
+    mfreq, maz, malt, J = _jones_matrix(model, frequency=frequency)
+    
+    # Set the "A" matrix
+    A = np.array([[1, 0,   0,  1],
+                  [1, 0,   0, -1],
+                  [0, 1,   1,  0],
+                  [0, 1j, -1j, 0]])
+                  
+    # Compute the Kronecker product of J and J*
+    M = np.zeros((4,4,J.shape[2],J.shape[3],J.shape[4]), dtype=J.dtype)
+    M[:2,:2,...] = J[0,0,...]*J.conj()
+    M[:2,2:,...] = J[0,1,...]*J.conj()
+    M[2:,:2,...] = J[1,0,...]*J.conj()
+    M[2:,2:,...] = J[1,1,...]*J.conj()
+    
+    # Swap the axis order to make the matrix multiplication easier
+    M = M.transpose(2,3,4,0,1)
+    
+    # Compute the Mueller matrix
+    M = np.matmul(A, np.matmul(M, np.linalg.inv(A)))
+    
+    # Another axis swap to put the polarization axes first
+    M = M.transpose(3,4,0,1,2)
+    
+    # Evaluate at each azimuth/altitude using a 3D interpolation function
+    ffreq = np.ones(az.size)*frequency
+    faz = az.ravel()
+    falt = alt.ravel()
+    resp = np.zeros((4,4)+faz.shape, dtype=np.float64)
+    for i in range(4):
+        for j in range(4):
+            P = M[i,j,...]
+            pfunc = RegularGridInterpolator((mfreq, malt, maz), P.real, bounds_error=False, fill_value=np.nan)
+            resp[i,j,:] = pfunc((ffreq, falt, faz))
+    resp.shape = (4,4)+az.shape
+    
+    return resp
+
+
+def _compute_from_feeds(pol, XE, XH, YE, YH):
+    """
+    Given a desired polarization product and a collection of feed response in
+    the E and H planes, return the combined response for that product.  The
+    returned array as the same dimensionallity as the input feed responses.
+    """
+    
+    pol = pol.upper()
+    if pol == 'XX':
+        return np.abs(XE)**2 + np.abs(XH)**2
+    elif pol == 'YY':
+        return np.abs(YE)**2 + np.abs(YH)**2
+    elif pol == 'XY':
+        return XE*YE.conj() + XH*YH.conj()
+    elif pol == 'YX':
+        return YE*XE.conj() + YH*XH.conj()
+    elif pol == 'I':
+        return _compute_from_feeds('XX', XE, XH, YE, YH) \
+               + _compute_from_feeds('YY', XE, XH, YE, YH)
+    elif pol == 'Q':
+        return _compute_from_feeds('XX', XE, XH, YE, YH) \
+               - _compute_from_feeds('YY', XE, XH, YE, YH)
+    elif pol == 'U':
+        return _compute_from_feeds('XY', XE, XH, YE, YH) \
+               + _compute_from_feeds('YX', XE, XH, YE, YH)
+    elif pol == 'V':
+        return _compute_from_feeds('XY', XE, XH, YE, YH) \
+               - _compute_from_feeds('YX', XE, XH, YE, YH)
+    else:
+        raise ValueErro(f"Unknown polarization produce '{pol}'")
+
+
+def beam_response(model, pol, az, alt, frequency=74e6, degrees=True):
+    """
+    Return the response of an isolated LWA dipole in a particular azimuth/
+    altitude, for the specified module source, polarization product, and
+    frequency.
+    """
+    
+    # Convert
+    if not degrees:
+        az = az * 180/np.pi
+        alt = alt * 180/np.pi
+        
+    # Load
+    model = model.lower()
+    if model == 'empirical':
+        mfreq, maz, malt, XE, XH, YE, YH = _load_response_fitted(frequency, corrected=False)
+    elif model == 'llfss':
+        mfreq, maz, malt, XE, XH, YE, YH = _load_response_fitted(frequency, corrected=True)
+    else:
+        mfreq, maz, malt, XE, XH, YE, YH = _load_response_full(frequency, model=model)
+        
+    # Build up the correct polarization product
+    P = _compute_from_feeds(pol, XE, XH, YE, YH)
+    pfunc = RegularGridInterpolator((mfreq, malt, maz), P.real, bounds_error=False, fill_value=np.nan)
+    
+    # Evaluate
+    ffreq = np.ones(az.size)*frequency
+    faz = az.ravel()
+    falt = alt.ravel()
+    resp = pfunc((ffreq, falt, faz))
+    resp.shape = az.shape
+    return resp
+
+
+def get_avaliable_models():
+    models = []
+    if os.path.exists(_EMP_FILENAME):
+        models.extend(['empirical', 'llfss'])
+    for model,filename in zip(('nec', 'feko', 'cst'), (_NEC_FILENAME, _FEKO_FILENAME, _CST_FILENAME)):
+        if os.path.exists(filename):
+            models.append(model)
+    
+    return models
