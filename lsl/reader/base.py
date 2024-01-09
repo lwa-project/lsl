921977b3a02e588fd8da5028353b7ce6ed012840
jaycedowell <jayce.dowell@gmail.com>
2024-01-09 10:52:05 -0700

    Switch over to astropy Time as the backend for the lsl.reader.base.FrameTimestamp class.

--- lsl/reader/base.py
+++ lsl/reader/base.py
@@ -12,13 +12,13 @@ from functools import total_ordering
 from textwrap import fill as tw_fill
 from datetime import datetime, timedelta
 
-from astropy.time import Time as AstroTime
+from astropy.time import Time as AstroTime, TimeDelta as AstroDelta
 
 from lsl.common import dp as dp_common
-from lsl.astro import unix_to_utcjd, MJD_OFFSET, unix_to_taimjd
+from lsl.astro import MJD_OFFSET
 
 
-__version__ = '0.3'
+__version__ = '0.4'
 __all__ = ['FrameHeaderBase', 'FramePayloadBase', 'FrameBase', 'FrameTimestamp', 'CI8']
 
 
@@ -335,23 +335,27 @@ class FrameTimestamp(object):
     """
     
     def __init__(self, si=0, sf=0.0):
-        if isinstance(si, (float, numpy.floating)):
-            sf = sf + (si - int(si))
-            si = int(si)
-        # Make sure sf is [0.0, 1.0)
-        if sf >= 1:
-            sfi = int(sf)
-            sff = sf - sfi
-            si += sfi
-            sf = sff
-        elif sf < 0:
-            sfi = int(sf) - 1
-            sff = sf - sfi
-            si += sfi
-            sf = sff
-        self._int = int(si)
-        self._frac = float(sf)
-        
+        if isinstance(si, AstroTime):
+            self._time = si
+            if sf != 0.0:
+                self._time += AstroDelta(sf, format='sec')
+        else:
+            if isinstance(si, (float, numpy.floating)):
+                sf = sf + (si - int(si))
+                si = int(si)
+            # Make sure sf is [0.0, 1.0)
+            if sf >= 1:
+                sfi = int(sf)
+                sff = sf - sfi
+                si += sfi
+                sf = sff
+            elif sf < 0:
+                sfi = int(sf) - 1
+                sff = sf - sfi
+                si += sfi
+                sf = sff
+            self._time = AstroTime(si, sf, format='unix', scale='utc')
+            
     @classmethod
     def now(cls):
         """
@@ -359,7 +363,7 @@ class FrameTimestamp(object):
         from `time.time()`.
         """
         
-        return cls(time.time())
+        return cls(AstroTime.now())
         
     @classmethod
     def from_dp_timetag(cls, value, offset=0):
@@ -379,156 +383,103 @@ class FrameTimestamp(object):
         Create a new FrameTimestamp from a MJD/MPM (milliseconds past midnight) pair.
         """
         
-        imjd = int(mjd)
-        fmjd = mjd - imjd
-        mpm = mpm + int(fmjd*86400*1000)
-        s =  mpm // 1000
-        f = (mpm - s*1000) / 1000.0
-        s = s + (imjd - 40587)*86400
-        return cls(s, f)
+        t = AstroTime(mjd, mpm//1000 / 86400, format='mjd', scale='utc')
+        f = (mpm % 1000) / 1000.0
+        return cls(t, f)
         
     @classmethod
     def from_pulsar_mjd(cls, mjd, mjd_frac, sec_frac):
         """
         Create a new FrameTimstamp from a three-element tuple of integer number 
-        of MJD days, fractional MJD day, and fractional seconds.
+        of MJD days, integer seconds since 0h on that day, and fractional seconds.
         """
         
-        s = (mjd - 40587)*86400
-        f = sec_frac
-        s1 = int(mjd_frac * 86400)
-        return cls(s+s1, f)
+        t = AstroTime(mjd, mjd_frac/86400, format='mjd', scale='utc')
+        return cls(t, sec_frac)
+        
+    @classmethod
+    def from_astropy(cls, t):
+        """
+        Create a new FrameTimestamp from a astropy.time.Time object.
+        """
+        
+        return cls(t, 0.0)
         
     def __str__(self):
-        dt = self.datetime
+        dt = self._time.datetime
         return str(dt)
         
     def __repr__(self):
-        return "<FrameTimestamp i=%i, f=%.9f>" % (self._int, self._frac)
+        t = self._time.unix
+        return "<FrameTimestamp i=%i, f=%.9f (%.9f %.9f)>" % (int(t), t-int(t), self._time.jd1, self._time.jd2)
         
     def __float__(self):
-        return self._int+self._frac
+        return self._time.unix
         
     def __getitem__(self, i):
+        t = self._time.unix
         if i == 0:
-            return self._int
+            return int(t)
         elif i == 1:
-            return self._frac
+            return t - int(t)
         else:
             raise IndexError
             
     def __add__(self, other):
         if isinstance(other, (int, float, numpy.integer, numpy.floating)):
-            oi = int(other)
-            of = other - oi
-            _int = self._int + oi
-            _frac = self._frac + of
-            if _frac >= 1:
-                _int += 1
-                _frac -= 1
-            return FrameTimestamp(_int, _frac)
+            t = self._time + AstroDelta(other, format='sec')
+            return FrameTimestamp(t)
         elif isinstance(other, timedelta):
-            oi = other.days*86400 + other.seconds
-            of = other.microseconds/1e6
-            _int = self._int + oi
-            _frac = self._frac + of
-            if _frac >= 1:
-                _int += 1
-                _frac -= 1
-            return FrameTimestamp(_int, _frac)
+            t = self._time + AstroDelta(other, format='datetime')
+            return FrameTimestamp(t)
         else:
             raise TypeError(f"Unsupported type: '{type(other).__name__}'")
             
     def __iadd__(self, other):
         if isinstance(other, (int, float, numpy.integer, numpy.floating)):
-            oi = int(other)
-            of = other - oi
-            self._int += oi
-            self._frac += of
-            if self._frac >= 1:
-                self._int += 1
-                self._frac -= 1
+            self._time += AstroDelta(other, format='sec')
             return self
         elif isinstance(other, timedelta):
-            oi = other.days*86400 + other.seconds
-            of = other.microseconds/1e6
-            self._int += oi
-            self._frac += of
-            if self._frac >= 1:
-                self._int += 1
-                self._frac -= 1
+            self._time += AstroDelta(other, format='datetime')
             return self
         else:
             raise TypeError(f"Unsupported type: '{type(other).__name__}'")
             
     def __sub__(self, other):
         if isinstance(other, FrameTimestamp):
-            oi, of = other[0], other[1]
-            _int = self._int - oi
-            _frac = self._frac - of
-            if _frac < 0:
-                _int -= 1
-                _frac += 1
-            return _int+_frac
+            diff = self._time - other._time
+            return diff.sec
         elif isinstance(other, (int, float, numpy.integer, numpy.floating)):
-            oi = int(other)
-            of = other - oi
-            _int = self._int - oi
-            _frac = self._frac - of
-            if _frac < 0:
-                _int -= 1
-                _frac += 1
-            return FrameTimestamp(_int, _frac)
+            t = self._time - AstroDelta(other, format='sec')
+            return FrameTimestamp(t)
         elif isinstance(other, timedelta):
-            oi = other.days*86400 + other.seconds
-            of = other.microseconds/1e6
-            _int = self._int - oi
-            _frac = self._frac - of
-            if _frac < 0:
-                _int -= 1
-                _frac += 1
-            return FrameTimestamp(_int, _frac)
+            t = self._time - AstroDelta(other, format='datetime')
+            return FrameTimestamp(t)
         else:
             raise TypeError(f"Unsupported type: '{type(other).__name__}'")
             
     def __isub__(self, other):
         if isinstance(other, (int, float, numpy.integer, numpy.floating)):
-            oi = int(other)
-            of = other - oi
-            self._int -= oi
-            self._frac -= of
-            if self._frac < 0:
-                self._int -= 1
-                self._frac += 1
+            self._time -= AstroDelta(other, format='sec')
             return self
         elif isinstance(other, timedelta):
-            oi = other.days*86400 + other.seconds
-            of = other.microseconds/1e6
-            self._int -= oi
-            self._frac -= of
-            if self._frac < 0:
-                self._int -= 1
-                self._frac += 1
+            self._time -= AstroDelta(other, format='datetime')
             return self
         else:
             raise TypeError(f"Unsupported type: '{type(other).__name__}'")
             
     def __eq__(self, y):
         if isinstance(y, FrameTimestamp):
-            return self._int == y._int and self._frac == y._frac
-        elif isinstance(y, (int, numpy.integer)):
-            return self._int == y and self._frac == 0.0
-        elif isinstance(y, (float, numpy.floating)):
-            return float(self) == float(y)
+            return self._time == y._time
+        elif isinstance(y, (int, numpy.integer, float, numpy.floating)):
+            return float(self) == y
         else:
             raise TypeError(f"Unsupported type: '{type(y).__name__}'")
             
     def __lt__(self, y):
         if isinstance(y, FrameTimestamp):
-            return (self._int < y._int) or (self._int == y._int and self._frac < y._frac)
-        elif isinstance(y, (int, numpy.integer)):
-            return self._int < y
-        elif isinstance(y, (float, numpy.floating)):
+            return self._time < y._time
+        elif isinstance(y, (int, numpy.integer, float, numpy.floating)):
             return float(self) < y
         else:
             raise TypeError(f"Unsupported type: '{type(y).__name__}'")
@@ -539,7 +490,7 @@ class FrameTimestamp(object):
         UNIX timestamp as a floating point value.
         """
         
-        return float(self)
+        return self._time.unix
         
     @property
     def jd(self):
@@ -547,7 +498,7 @@ class FrameTimestamp(object):
         UTC JD as a floating point value.
         """
         
-        return unix_to_utcjd(self)
+        return self._time.jd
         
     @property
     def mjd(self):
@@ -555,18 +506,45 @@ class FrameTimestamp(object):
         UTC MJD as a floating point value.
         """
         
-        return self.jd - MJD_OFFSET
+        return self._time.mjd
         
     @property
     def pulsar_mjd(self):
         """
         UTC MJD as  three-element tuple of integer number of MJD days,
-        fractional MJD day, and fractional seconds.
+        integer number of seconds since 0h on that day, and fractional seconds.
         """
         
-        days = self._int // 86400
-        frac = (self._int - days*86400) / 86400.0
-        return (days + 40587, frac, self._frac)
+        j1i = int(self._time.jd1)
+        j1f = self._time.jd1 - j1i
+        j2i = int(self._time.jd2)
+        j2f = self._time.jd2 - j2i
+        
+        mjd = j1i + j2i - MJD_OFFSET
+        mjd_frac1 = j1f + (mjd - int(mjd))
+        mjd_frac2 = j2f
+        mjd = int(mjd)
+        while mjd_frac1 >= 1.0:
+            mjd += 1
+            mjd_frac1 -= 1
+        while mjd_frac2 >= 1.0:
+            mjd += 1
+            mjd_frac2 -= 1
+        while mjd_frac1 < 0.0:
+            mjd -= 1
+            mjd_frac1 += 1
+        while mjd_frac2 < 0.0:
+            mjd -= 1
+            mjd_frac2 += 1
+            
+        day_sec1 = mjd_frac1 * 86400
+        day_sec2 = mjd_frac2 * 86400
+        day_sec = int(day_sec1) + int(day_sec2)
+        if day_sec > 86400:
+            mjd += 1
+            day_sec -= 86400
+        sec_frac = (day_sec1 - int(day_sec1)) + (day_sec2 - int(day_sec2))
+        return mjd, day_sec, sec_frac
         
     @property
     def tai_mjd(self):
@@ -574,7 +552,7 @@ class FrameTimestamp(object):
         TAI MJD as a floating point value.
         """
         
-        return unix_to_taimjd(self)
+        return self._time.tai.mjd
         
     @property
     def dp_timetag(self):
@@ -583,8 +561,11 @@ class FrameTimestamp(object):
         on January 1, 1970).
         """
         
-        tt = self._int * int(dp_common.fS)
-        tt = tt + int(self._frac*dp_common.fS)
+        sec = int(self._time.unix)
+        sec_frac = self._time - AstroTime(sec, format='unix', scale='utc')
+        
+        tt = sec * int(dp_common.fS)
+        tt = tt + int(round(sec_frac.sec*dp_common.fS, 2))
         return tt
         
     @property
@@ -593,14 +574,7 @@ class FrameTimestamp(object):
         Timestamp as a naive `datetime.datetime` instance in UTC.
         """
         
-        s = self._int
-        us = int(self._frac*1e6)
-        if us >= 1000000:
-            s += 1
-            us -= 1000000
-        dt = datetime.utcfromtimestamp(s)
-        dt += timedelta(microseconds=us)
-        return dt
+        return self._time.datetime
         
     @property
     def utc_datetime(self):
@@ -616,7 +590,7 @@ class FrameTimestamp(object):
         Timestamp as an `astropy.time.Time` instance.
         """
         
-        return AstroTime(self._int, self._frac, format='unix', scale='utc')
+        return self._time
 
 
 CI8 = numpy.dtype([('re', 'i1'), ('im', 'i1')])
