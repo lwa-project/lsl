536aa52b616aebafa7ebbcc75ed5943774a74b72
jaycedowell <jaycedowell@gmail.com>
2024-02-14 13:04:16 -0700

    Updates for TRK_LUN.

--- tests/test_mcs.py
+++ tests/test_mcs.py
@@ -79,10 +79,10 @@ class mcs_tests(unittest.TestCase):
     def test_mode_limits(self):
         """Test valid observing mode values"""
         
-        for i in range(1, 8+1):
+        for i in range(1, 9+1):
             mcs.mode_to_string(i)
         self.assertRaises(ValueError, mcs.mode_to_string, 0)
-        self.assertRaises(ValueError, mcs.mode_to_string, 9)
+        self.assertRaises(ValueError, mcs.mode_to_string, 10)
         
     def test_pointing_correction(self):
         """Test the pointing correction function"""
