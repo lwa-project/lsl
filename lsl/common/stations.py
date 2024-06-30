39e8fbff04b0b31727dd9566c3bec25f054c4761
jaycedowell <jayce.dowell@gmail.com>
2024-06-30 10:50:51 -0600

    Do we need an obstime to fix Py3.6?

--- lsl/common/stations.py
+++ lsl/common/stations.py
@@ -643,9 +643,9 @@ class Stand(object):
             aa = aa.transform_to(ITRS(location=center))
             aa = ITRS(aa.cartesian.xyz + center.itrs.cartesian.xyz)
         except TypeError:
-            aa = aa.transform_to(ITRS())
-            rf = AltAz(CartesianRepresentation('0.01mm', '0.01mm', '0.01mm'), location=center)
-            rf = rf.transform_to(ITRS())
+            aa = aa.transform_to(ITRS(obstime='J2000'))
+            rf = AltAz(CartesianRepresentation('0.01mm', '0.01mm', '0.01mm'), location=center, obstime='J2000')
+            rf = rf.transform_to(ITRS(obstime='J2000'))
             aa = ITRS(aa.cartesian.xyz + (center.itrs.cartesian.xyz-rf.cartesian.xyz), obstime='J2000')
         return EarthLocation.from_geocentric(aa.x, aa.y, aa.z)
 
