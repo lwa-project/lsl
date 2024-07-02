6620462ac474af2eebd5e34e0175d7c69f8444ee
jaycedowell <jayce.dowell@gmail.com>
2024-07-02 10:42:47 -0600

    Fix the astropy version.

--- setup.py
+++ setup.py
@@ -388,7 +388,7 @@ setup(
     scripts = glob.glob('scripts/*.py'), 
     python_requires='>=3.8', 
     setup_requires = ['numpy>=1.7'], 
-    install_requires = ['astropy>', 'jplephem', 'healpy', 'numpy>=1.7', 'scipy>=0.19', 'pyephem>=3.7.5.3', 'aipy>=3.0.1', 'pytz>=2012c'],
+    install_requires = ['astropy>=5.1', 'jplephem', 'healpy', 'numpy>=1.7', 'scipy>=0.19', 'pyephem>=3.7.5.3', 'aipy>=3.0.1', 'pytz>=2012c'],
     include_package_data = True,  
     ext_package = 'lsl', 
     ext_modules = ExtensionModules,
