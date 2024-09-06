f8676d1d33e9d59e02168f47ffbfc2416e45f620
jaycedowell <jayce.dowell@gmail.com>
2024-09-06 13:36:09 -0600

    Include cstdint for gcc13.

--- lsl/reader/drx8.cpp
+++ lsl/reader/drx8.cpp
@@ -1,8 +1,6 @@
 #include "Python.h"
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <complex.h>
+#include <cmath>
+#include <cstdint>
 
 #define NO_IMPORT_ARRAY
 #define PY_ARRAY_UNIQUE_SYMBOL gofast_ARRAY_API
