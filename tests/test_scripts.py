a388df8e200f42ba1feaebc2bb110fcaa5904423
jaycedowell <jayce.dowell@gmail.com>
2024-09-18 11:16:30 -0600

    Switch to JSON for pylint to make the parsing easier.

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -5,8 +5,8 @@ Unit tests for the various LSL scripts.
 import unittest
 import glob
 import sys
-import re
 import os
+import json
 
 from lsl.common.paths import MODULE_BUILD
 
@@ -14,17 +14,14 @@ run_scripts_tests = False
 try:
     from io import StringIO
     from pylint.lint import Run
-    from pylint.reporters.text import TextReporter
+    from pylint.reporters.json_reporter import JSONReporter
     run_scripts_tests = True
 except ImportError:
     pass
 
 
-__version__  = "0.1"
+__version__  = "0.2"
 __author__   = "Jayce Dowell"
-
-
-_LINT_RE = re.compile(r'(?P<module>.*?):(?P<line>\d+): \[(?P<type>.*?)\] (?P<info>.*)')
 
 
 @unittest.skipUnless(run_scripts_tests, "requires the 'pylint' module")
@@ -38,23 +35,16 @@ class scripts_tests(unittest.TestCase):
         _SCRIPTS.sort()
         for script in _SCRIPTS:
             name = script.rsplit('scripts')[-1]
-            with self.subTest(script=name):
+            with self.subTest(script=name.replace(os.path.sep, '')):
                 pylint_output = StringIO()
-                reporter = TextReporter(pylint_output)
+                reporter = JSONReporter(pylint_output)
                 pylint_args = [script, "-E", "--extension-pkg-whitelist=numpy", "--init-hook='import sys; sys.path=[%s]; sys.path.insert(0, \"%s\")'" % (",".join(['"%s"' % p for p in sys.path]), os.path.dirname(MODULE_BUILD))]
                 Run(pylint_args, reporter=reporter, exit=False)
-                out = pylint_output.getvalue()
-                out_lines = out.split('\n')
+                results = json.loads(pylint_output.getvalue())
                 
-                for line in out_lines:
-                    mtch = _LINT_RE.match(line)
-                    if mtch is not None:
-                        line_no, type, info = mtch.group('line'), mtch.group('type'), mtch.group('info')
-                        self.assertEqual(type.find('syntax'), -1, "%s:%s - %s" % (os.path.basename(script), line_no, info))
-                        self.assertEqual(type.find('undefined'), -1, "%s:%s - %s" % (os.path.basename(script), line_no, info))
-                        # HACK to deal with some strangeness in lwa_cat_view.py
-                        if script.find('lwa_cat_view.py') == -1 or (script.find('lwa_cat_view.py') != -1 and info.find('j2000_') == -1):
-                            self.assertEqual(type.find('no-member'), -1, "%s:%s - %s" % (os.path.basename(script), line_no, info))
+                for i,entry in enumerate(results):
+                    with self.subTest(error_number=i+1):
+                        self.assertTrue(False, f"{entry['path']}:{entry['line']} - {entry['message']}")
 
 
 class scripts_test_suite(unittest.TestSuite):
